8c96e13a2c844413dd9074493c4e2f9bbd84ee57
mehthishamkhan06 <mehthishamkhan06@gmail.com>
2024-05-30 17:31:46 +0400

    5

---
+++ ft_strmapi.c
@@ -0,0 +1,66 @@
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int i;
+	char *str;
+
+	i = 0;
+	if (s[i] == '\0')
+		return (NULL);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (str == NULL)
+		return (NULL);
+	while(s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// // Prototype declaration for ft_strmap1 function
+// char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
+
+// // Example transformation function: Converts lowercase letters to uppercase
+// char to_uppercase(unsigned int index, char c)
+// {
+//     if (c >= 'a' && c <= 'z')
+//     {
+//         // Convert lowercase letter to uppercase
+//         return c - 32;
+//     }
+//     else
+//     {
+//         // Return unchanged for non-lowercase letters
+//         return c;
+//     }
+// }
+
+// int main()
+// {
+//     char input[] = "Hello, World!";
+//     char *result;
+
+//     // Test ft_strmap1 with the to_uppercase function
+//     result = ft_strmapi(input, to_uppercase);
+
+//     if (result)
+//     {
+//         printf("Original string: %s\n", input);
+//         printf("Transformed string: %s\n", result);
+
+//         // Remember to free the memory allocated by ft_strmap1
+//         free(result);
+//     }
+//     else
+//     {
+//         printf("Memory allocation failed.\n");
+//     }
+
+//     return 0;
+// }
