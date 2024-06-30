e460364f858ba2da2add9d6f1c13e658b7d8a9e2
Mohammed Ehthisham Ullah Khan <mohkhan@lab1r6s4.42abudhabi.ae>
2024-06-30 11:18:13 +0400

    int main deleted

--- ft_memmove.c
+++ ft_memmove.c
@@ -34,30 +34,3 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	return (dest);
 }
 
-// void    *ft_memmove(void *dest, const void *src, size_t n)
-// {
-//     unsigned char   *a;
-//     unsigned char   *b;
-//     size_t             sub;
-//     size_t             len;
-
-//     sub = (unsigned char *)src - (unsigned char *)dest;
-//     len = (int)n;
-//     if (dest == src)
-//         return (dest);
-//     if (sub < len && sub > 0)
-//         return (ft_memcpy(dest, src, n));
-//     else
-//     {
-//         a = (unsigned char *)dest + len - 1;
-//         b = (unsigned char *)src + len - 1;
-//         while(n)
-//         {
-//             *a-- = *b--;
-//             n--;
-//         }
-//         a++;
-//         b++;
-//     }
-//     return (dest);
-// }
